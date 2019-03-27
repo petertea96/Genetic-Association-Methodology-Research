205edca6e9f97067445ad72b290cdb114fde3902
PeterTea <petertea96@gmail.com>
2019-03-26 22:25:37 -0400

    Added pvalue dotted line on density plots for focal analysis

--- Crohns Disease/Allele Kernel Analysis/Focal Point Analysis/Density Plots.R
+++ Crohns Disease/Allele Kernel Analysis/Focal Point Analysis/Density Plots.R
@@ -14,6 +14,7 @@ Stacked_SKAT = melt(data=SKAT, id.vars = 1)
 
 library(ggplot2)
 ggplot(Stacked_SKAT, aes(x=value, y = ..scaled.., fill=variable)) + geom_density() + facet_wrap(~variable) +
+  geom_vline(xintercept = 0.05, show.legend = TRUE, linetype = "dashed", color = "red") +
   ggtitle("SKAT - Allele/Genotype kernel association distributions") +
   xlab("Allele/Genotype kernel") + ylab("Statistic Density") +
   theme_classic() +
@@ -30,6 +31,7 @@ Stacked_MDMR = melt(data=MDMR, id.vars = 1)
 
 library(ggplot2)
 ggplot(Stacked_MDMR, aes(x=value, y = ..scaled.., fill=variable)) + geom_density() + facet_wrap(~variable) +
+  geom_vline(xintercept = 0.05, show.legend = TRUE, linetype = "dashed", color = "red") +
   ggtitle("MDMR - Allele/Genotype kernel association distributions") +
   xlab("Allele/Genotype kernel") + ylab("Statistic Density") +
   theme_classic() +
@@ -45,6 +47,7 @@ Stacked_GTSM = melt(data=GTSM, id.vars = 1)
 
 library(ggplot2)
 ggplot(Stacked_GTSM, aes(x=value, y = ..scaled.., fill=variable)) + geom_density() + facet_wrap(~variable) +
+  geom_vline(xintercept = 0.05, show.legend = TRUE, linetype = "dashed", color = "red") +
   ggtitle("GTSM - Allele/Genotype kernel association distributions") +
   xlab("Allele/Genotype kernel") + ylab("Statistic Density") +
   theme_classic() +
